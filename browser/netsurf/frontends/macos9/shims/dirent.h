bed8fa89c63f205accdc66938274dc060528a3af
Patrick <patrick@mp.ls>
2026-04-10 00:14:15 +0200

    Build 6 fixes: stat.h guard, string.h in prefix, mac_dirent.h, inttypes macros
    - Block MSL stat.h with _MSL_STAT_H/__stat_h/_STAT_H guards in our shim
    - Use MSL's hex values (0xF000, 0x4000, 0x8000) for S_IF* macros
    - Guard all S_IF*/S_IS* macros with #ifndef to prevent redefinition
    - Add struct dirent, DIR, opendir/readdir/closedir to mac_dirent.h
    - Redirect dirent.h shim to mac_dirent.h (single canonical definition)
    - Add PRIu16, PRIX32, PRIxPTR to inttypes.h shim
    - Fix PRIsizet/PRIssizet to "lu"/"ld" for PPC (not "zu"/"zd")
    - string.h already in prefix (verified, no change needed)

--- browser/netsurf/frontends/macos9/shims/dirent.h
+++ browser/netsurf/frontends/macos9/shims/dirent.h
@@ -1,14 +1,9 @@
 #ifndef MACSURF_DIRENT_H
 #define MACSURF_DIRENT_H
 
-typedef void DIR;
-struct dirent {
-    char d_name[256];
-};
+/* Redirect to mac_dirent.h which provides the canonical definitions */
+#include "mac_dirent.h"
 
-DIR *opendir(const char *name);
-struct dirent *readdir(DIR *dirp);
-int closedir(DIR *dirp);
 int alphasort(const struct dirent **d1, const struct dirent **d2);
 int scandir(const char *dirp, struct dirent ***namelist,
   int (*filter)(const struct dirent *),
